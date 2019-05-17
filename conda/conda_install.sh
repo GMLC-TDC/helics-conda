fefe3381728a24e078c69cef4e4d784ca87da1af
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2019-05-17 14:25:48 -0600

    Windows on Travis

--- conda/conda_install.sh
+++ conda/conda_install.sh
@@ -2,11 +2,21 @@
 
 set -ev # exit on first error, print each command
 
+if [[ "$TRAVIS_OS_NAME" != "windows" ]]; then
+    export MINICONDA=$HOME/miniconda
+else
+    export MINICONDA=/c/miniconda
+fi
+
 if [ ${PYTHON_ARCH} == "32" ]; then
   set CONDA_FORCE_32BIT=1
 fi
-conda create -q -n helics-build-environment python=$PYTHON_VERSION
-source activate helics-build-environment
+
+hash -r
+conda update -q conda
 conda info -a
+
+conda create -n helics-build-environment python=$PYTHON_VERSION
+source activate helics-build-environment
 conda list
 
--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -1,10 +1,17 @@
-# Only need to change these two variables
 PKG_NAME=helics
 
-OS=$TRAVIS_OS_NAME-64
+if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
+    OS="win-64"
+else
+    OS=$TRAVIS_OS_NAME-64
+fi
+
 mkdir ~/conda-bld
-conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
-export VERSION="2.0.0-rc1"
-conda build $PKG_NAME
+export VERSION="2.0.0"
+echo "Started building ..."
+conda build $PKG_NAME --no-test
+echo "Finished building. Uploading ..."
 anaconda -t $ANACONDA_TOKEN upload $CONDA_BLD_PATH/$OS/$PKG_NAME-*.tar.bz2 --force
+echo "Finished uploading."
+
