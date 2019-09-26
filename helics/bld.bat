8b249aebdc841081c6f840889687bf414af2cdae
Dheepak Krishnamurthy <me@kdheepak.com>
2019-09-26 16:01:46 -0600

    Update files with cmake -DPYTHON_* variables

--- helics/bld.bat
+++ helics/bld.bat
@@ -17,10 +17,10 @@ set PY_VER_NO_DOT=%PY_VER:.=%
 
 if %PYTHON_ARCH% equ 64 (
   echo "Building 64 bit"
-  cmake -DZMQ_LOCAL_BUILD=ON  -DENABLE_SWIG=ON  -DCMAKE_BUILD_TYPE=Release -A x64 -DCOMPILER_CAN_DO_CPP_11=ON -DCXX_FLAGS="-std=c++11" -DBUILD_HELICS_TESTS=OFF -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" %BUILD_PYTHON%" ..
+  cmake -DZMQ_LOCAL_BUILD=ON  -DENABLE_SWIG=ON  -DCMAKE_BUILD_TYPE=Release -A x64 -DCOMPILER_CAN_DO_CPP_11=ON -DCXX_FLAGS="-std=c++11" -DBUILD_HELICS_TESTS=OFF -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" %BUILD_PYTHON%" -DPYTHON_INCLUDE_DIR=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") -DPYTHON_LIBRARY=$(python -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR'))") -DPYTHON_EXECUTABLE=$(where python) ..
 ) else (
   echo "Building 32 bit"
-  cmake -DZMQ_LOCAL_BUILD=ON  -DENABLE_SWIG=ON  -DCMAKE_BUILD_TYPE=Release -DCOMPILER_CAN_DO_CPP_11=ON -DCXX_FLAGS="-std=c++11" -DBUILD_HELICS_TESTS=OFF -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" %BUILD_PYTHON%" ..
+  cmake -DZMQ_LOCAL_BUILD=ON  -DENABLE_SWIG=ON  -DCMAKE_BUILD_TYPE=Release -DCOMPILER_CAN_DO_CPP_11=ON -DCXX_FLAGS="-std=c++11" -DBUILD_HELICS_TESTS=OFF -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" %BUILD_PYTHON%" -DPYTHON_INCLUDE_DIR=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") -DPYTHON_LIBRARY=$(python -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR'))") -DPYTHON_EXECUTABLE=$(where python) ..
 )
 cmake --build . --config Release --target install
 
