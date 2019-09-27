94c2849d6dd4b1da4b0a3ffd5b091dd9ec4d526c
Dheepak Krishnamurthy <me@kdheepak.com>
2019-09-27 11:31:43 -0600

    Update helics/bld.bat with correct variables

--- helics/bld.bat
+++ helics/bld.bat
@@ -8,8 +8,24 @@ git clean -fxd
 
 set PY_VER_NO_DOT=%PY_VER:.=%
 
+python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())" > tempFile
+set /p HELICS_PYTHON_INCLUDE_DIR= < tempFile
+del tempFile
+
+python -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR'))" > tempFile
+set /p HELICS_PYTHON_LIBRARY= < tempFile
+del tempFile
+
+where python > tempFile
+set /p HELICS_PYTHON_EXECUTABLE= < tempFile
+del tempFile
+
+echo %HELICS_PYTHON_INCLUDE_DIR%
+echo %HELICS_PYTHON_LIBRARY%
+echo %HELICS_PYTHON_EXECUTABLE%
+
 echo "Building 64 bit"
-cmake -DZMQ_LOCAL_BUILD=ON  -DENABLE_SWIG=ON  -DCMAKE_BUILD_TYPE=Release -A x64 -DBUILD_HELICS_TESTS=OFF -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" %BUILD_PYTHON%" -DPYTHON_INCLUDE_DIR=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") -DPYTHON_LIBRARY=$(python -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR'))") -DPYTHON_EXECUTABLE=$(where python) ..
+cmake -DZMQ_LOCAL_BUILD=ON  -DENABLE_SWIG=ON  -DCMAKE_BUILD_TYPE=Release -A x64 -DBUILD_HELICS_TESTS=OFF -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" %BUILD_PYTHON%" -DPYTHON_INCLUDE_DIR=%HELICS_PYTHON_INCLUDE_DIR% -DPYTHON_LIBRARY=%HELICS_PYTHON_LIBRARY% -DPYTHON_EXECUTABLE=%HELICS_PYTHON_EXECUTABLE% ..
 cmake --build . --config Release --target install
 
 xcopy %LIBRARY_PREFIX%\python\* %SP_DIR%
