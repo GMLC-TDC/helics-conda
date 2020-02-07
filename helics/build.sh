7a9eab6d0e961feb0ca5c2cad403ae9f1a846a1a
Dheepak Krishnamurthy <me@kdheepak.com>
2020-02-07 14:24:29 -0700

    Hard code whl for linux

--- helics/build.sh
+++ helics/build.sh
@@ -4,9 +4,7 @@ which pip
 which python
 pip --version
 python --version
-pip install pip --upgrade
-
 python-config --extension-suffix
 
 # install using pip from the whl file
-pip install helics -vv
+pip install https://files.pythonhosted.org/packages/00/a1/e965807ba1241af2b7d81c4404ca15805c0d94188508ba9c5dda78b2d74e/helics-2.4.0-cp37-cp37m-manylinux2010_x86_64.whl -vv
