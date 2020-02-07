29a8f096a25c8eba9b4073dd089656ffb30a2cd3
Dheepak Krishnamurthy <me@kdheepak.com>
2020-02-07 14:38:32 -0700

    Add multiple versions

---
+++ helics/bld.bat
@@ -0,0 +1,17 @@
+IF "%PY_VER%"=="2.7" (
+	%PYTHON% -m pip install --no-deps https://pypi.org/packages/cp27/h/helics/helics-%PKG_VERSION%-cp27-cp27mu-win_amd64.whl
+)
+
+IF "%PY_VER%"=="3.5" (
+	%PYTHON% -m pip install --no-deps https://pypi.org/packages/cp35/h/helics/helics-%PKG_VERSION%-cp35-cp35m-win_amd64.whl
+)
+
+IF "%PY_VER%"=="3.6" (
+	%PYTHON% -m pip install --no-deps https://pypi.org/packages/cp36/h/helics/helics-%PKG_VERSION%-cp36-cp36m-win_amd64.whl
+)
+
+IF "%PY_VER%"=="3.7" (
+	%PYTHON% -m pip install --no-deps https://pypi.org/packages/cp37/h/helics/helics-%PKG_VERSION%-cp37-cp37m-win_amd64.whl
+)
+
+if errorlevel 1 exit 1
--- helics/build.sh
+++ helics/build.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-which pip
-which python
-pip --version
-python --version
-python-config --extension-suffix
 
-# install using pip from the whl file
-pip install https://files.pythonhosted.org/packages/00/a1/e965807ba1241af2b7d81c4404ca15805c0d94188508ba9c5dda78b2d74e/helics-2.4.0-cp37-cp37m-manylinux2010_x86_64.whl -vv
+if [ `uname` == Darwin ]; then
+    if [ "$PY_VER" == "2.7" ]; then
+        WHL_FILE=https://pypi.org/packages/cp27/h/helics/helics-${PKG_VERSION}-cp27-cp27mu-macosx_10_9_intel.whl
+    elif [ "$PY_VER" == "3.5" ]; then
+        WHL_FILE=https://pypi.org/packages/cp35/h/helics/helics-${PKG_VERSION}-cp35-cp35m-macosx_10_9_intel.whl
+    elif [ "$PY_VER" == "3.6" ]; then
+        WHL_FILE=https://pypi.org/packages/cp36/h/helics/helics-${PKG_VERSION}-cp36-cp36m-macosx_10_9_intel.whl
+    elif [ "$PY_VER" == "3.7" ]; then
+        WHL_FILE=https://pypi.org/packages/cp37/h/helics/helics-${PKG_VERSION}-cp37-cp37m-macosx_10_9_intel.whl
+    fi
+fi
+
+if [ `uname` == Linux ]; then
+    if [ "$PY_VER" == "2.7" ]; then
+        WHL_FILE=https://pypi.org/packages/cp27/h/helics/helics-${PKG_VERSION}-cp27-cp27mu-manylinux2010_x86_64.whl
+    elif [ "$PY_VER" == "3.5" ]; then
+        WHL_FILE=https://pypi.org/packages/cp35/h/helics/helics-${PKG_VERSION}-cp35-cp35m-manylinux2010_x86_64.whl
+    elif [ "$PY_VER" == "3.6" ]; then
+        WHL_FILE=https://pypi.org/packages/cp36/h/helics/helics-${PKG_VERSION}-cp36-cp36m-manylinux2010_x86_64.whl
+    elif [ "$PY_VER" == "3.7" ]; then
+        WHL_FILE=https://pypi.org/packages/cp37/h/helics/helics-${PKG_VERSION}-cp37-cp37m-manylinux2010_x86_64.whl
+    fi
+fi
+
+pip install --no-deps $WHL_FILE
