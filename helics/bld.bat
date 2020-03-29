3639c97ee7366e88f87d1b66ad782e38ff2b6648
Dheepak Krishnamurthy <me@kdheepak.com>
2020-03-29 08:51:58 -0600

    Update to v2.4.2

--- helics/bld.bat
+++ helics/bld.bat
@@ -15,14 +15,14 @@ IF "%PY_VER%"=="3.7" (
 )
 
 IF "%ARCH%"=="32" (
-    %PYTHON% -m pip install --no-deps https://files.pythonhosted.org/packages/18/0d/bb3fe292beb8f602dc0ac6f2ca62ad9d0ec067c7d1006690ef4fbbafc93d/helics_apps-2.4.1-py2.py3-none-win32.whl
+    %PYTHON% -m pip install --no-deps https://pypi.org/packages/py2.py3/h/helics_apps/helics_apps-%PKG_VERSION%-py2.py3-none-win32.whl
 ) ELSE (
-    %PYTHON% -m pip install --no-deps https://files.pythonhosted.org/packages/fa/5c/8db7fecc8fbd0b0aa1418e727f1dcd22dedd495228902a60be6fd9e122ab/helics_apps-2.4.1-py2.py3-none-win_amd64.whl
+    %PYTHON% -m pip install --no-deps https://pypi.org/packages/py2.py3/h/helics_apps/helics_apps-%PKG_VERSION%-py2.py3-none-win_amd64.whl
 )
 IF "%ARCH%"=="64" (
-    %PYTHON% -m pip install --no-deps https://files.pythonhosted.org/packages/fa/5c/8db7fecc8fbd0b0aa1418e727f1dcd22dedd495228902a60be6fd9e122ab/helics_apps-2.4.1-py2.py3-none-win_amd64.whl
+    %PYTHON% -m pip install --no-deps https://pypi.org/packages/py2.py3/h/helics_apps/helics_apps-%PKG_VERSION%-py2.py3-none-win_amd64.whl
 ) ELSE (
-    %PYTHON% -m pip install --no-deps https://files.pythonhosted.org/packages/18/0d/bb3fe292beb8f602dc0ac6f2ca62ad9d0ec067c7d1006690ef4fbbafc93d/helics_apps-2.4.1-py2.py3-none-win32.whl
+    %PYTHON% -m pip install --no-deps https://pypi.org/packages/py2.py3/h/helics_apps/helics_apps-%PKG_VERSION%-py2.py3-none-win32.whl
 )
 
 if errorlevel 1 exit 1
--- helics/build.sh
+++ helics/build.sh
@@ -10,7 +10,7 @@ if [ `uname` == Darwin ]; then
     elif [ "$PY_VER" == "3.7" ]; then
         HELICS_PY_WHL_FILE=https://pypi.org/packages/cp37/h/helics/helics-${PKG_VERSION}-cp37-cp37m-macosx_10_9_intel.whl
     fi
-    HELICS_APPS_WHL_FILE=https://files.pythonhosted.org/packages/ca/d5/f8a405222f4f4254e424b99959dcb2775923d96debaea611dd101fa701e3/helics_apps-2.4.1-py2.py3-none-macosx_10_9_intel.whl
+    HELICS_APPS_WHL_FILE=https://pypi.org/packages/py2.py3/h/helics_apps/helics_apps-${PKG_VERSION}-py2.py3-none-macosx_10_9_intel.whl
 fi
 
 if [ `uname` == Linux ]; then
@@ -23,7 +23,7 @@ if [ `uname` == Linux ]; then
     elif [ "$PY_VER" == "3.7" ]; then
         HELICS_PY_WHL_FILE=https://pypi.org/packages/cp37/h/helics/helics-${PKG_VERSION}-cp37-cp37m-manylinux2010_x86_64.whl
     fi
-    HELICS_APPS_WHL_FILE=https://files.pythonhosted.org/packages/11/a1/a59d2fffb34c762fd4e96496a9eb38c27edf6d5158fff804e0f9fa8bc757/helics_apps-2.4.1-py2.py3-none-manylinux2010_x86_64.whl
+    HELICS_APPS_WHL_FILE=https://pypi.org/packages/py2.py3/h/helics_apps/helics_apps-${PKG_VERSION}-py2.py3-none-manylinux2010_x86_64.whl
 fi
 
-pip install --no-deps $HELICS_PY_WHL_FILE $HELICS_APPS_WHL_FILE
+pip install --no-cache-dir --disable-pip-version-check --no-deps $HELICS_PY_WHL_FILE $HELICS_APPS_WHL_FILE
