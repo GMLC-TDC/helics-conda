ed461e8479c38330fc321c0dace5fc5d24ce4bc7
Dheepak Krishnamurthy <me@kdheepak.com>
2020-10-30 13:47:17 -0600

    Uncomment helics build

--- helics/build.sh
+++ helics/build.sh
@@ -17,10 +17,10 @@ echo "Installing helics to ${PREFIX}"
 ls
 
 mkdir -p $SRC_DIR/helics/build && cd $SRC_DIR/helics/build
-# git submodule update
-# cmake $CONDA_CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=$FLAGS -DCMAKE_INSTALL_PREFIX=$PREFIX ../
-# make -j $CPU_COUNT
-# make install
+git submodule update
+cmake $CONDA_CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=$FLAGS -DCMAKE_INSTALL_PREFIX=$PREFIX ../
+make -j $CPU_COUNT
+make install
 
 if [[ $PY3K -eq 1 || $PY3K == True || $PY3K == "True" ]]; then
     echo "Python 3"
