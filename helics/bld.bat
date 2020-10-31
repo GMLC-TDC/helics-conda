4f466a3c3769f3affb97a053324acfc9e346a1c7
Dheepak Krishnamurthy <me@kdheepak.com>
2020-10-31 16:46:13 -0600

    Update bld.bat

--- helics/bld.bat
+++ helics/bld.bat
@@ -1,13 +1,36 @@
-echo "******************* Installing PyHELICS ***************"
+mkdir "%SRC_DIR%"\build
+pushd "%SRC_DIR%"\build
 
-cd $SRC_DIR/pyhelics
+set PY_VER_NO_DOT=%PY_VER:.=%
 
-mkdir -p "$SP_DIR/helics/install"
-cp -r helics/* $SP_DIR/helics/
+where python > tempFile
+set /p HELICS_PYTHON_PREFIX= < tempFile
+del tempFile
 
-echo "******************* Installing HELICS ***************"
+python -c "import os, sys; print(os.path.dirname(sys.argv[1]))" %HELICS_PYTHON_PREFIX% > tempFile
+set /p HELICS_PYTHON_PREFIX= < tempFile
+del tempFile
 
-mkdir -p $SRC_DIR/helics/build
-cd $SRC_DIR/helics/build
-git submodule update
-cmake -DCMAKE_INSTALL_PREFIX="$SP_DIR/helics/install" --build . --target install --config Release
+echo "Python location:"
+echo %HELICS_PYTHON_PREFIX%
+
+echo "Setting build options"
+
+if "%PYTHON_ARCH%" == "32" (
+    set CONDA_FORCE_32BIT=1
+    set HELICS_PLATFORM=-AWin32
+    echo "Building 32 bit"
+) ELSE (
+    set HELICS_PLATFORM=-Ax64
+    echo "Building 64 bit"
+)
+
+
+echo %HELICS_PLATFORM%
+
+cmake -DCMAKE_BUILD_TYPE=Release -DHELICS_BUILD_TESTS=OFF -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
+cmake --build . --config Release --target install
+
+xcopy %LIBRARY_PREFIX%\python\* %SP_DIR%
+
+popd
