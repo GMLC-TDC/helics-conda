346a15e95ba1afc084813f7e277e92b6c09f5003
Dheepak Krishnamurthy <me@kdheepak.com>
2020-10-31 13:17:20 -0600

    Update build.sh

--- helics/build.sh
+++ helics/build.sh
@@ -30,4 +30,4 @@ cd $SRC_DIR/pyhelics
 
 python setup.py install --single-version-externally-managed --record=record.txt
 
-cp -v ${PREFIX}/* ${SP_DIR}/helics/install
+cp -rv ${PREFIX}/* ${SP_DIR}/helics/install
