37a21447646d7993ba6ffad4c8801eb188c1b2de
Dheepak Krishnamurthy <me@kdheepak.com>
2020-10-31 17:12:33 -0600

    Add error code for bld.bat

--- helics/bld.bat
+++ helics/bld.bat
@@ -23,6 +23,8 @@ mkdir "%SP_DIR%"\helics\install
 cmake -G"Ninja" -DCMAKE_BUILD_TYPE=Release -DHELICS_BUILD_TESTS=OFF -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX=%SP_DIR%\helics\install ..
 cmake --build . --config Release --target install
 
+if errorlevel 1 exit /b 1
+
 popd
 
 pushd "%SRC_DIR%"\pyhelics\
@@ -30,3 +32,5 @@ pushd "%SRC_DIR%"\pyhelics\
 xcopy helics\* %SP_DIR%\helics\
 
 popd
+
+exit 0
