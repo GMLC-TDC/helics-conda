fa7e4eaa4a1de17b3ea278878d98f1842fbb497c
Dheepak Krishnamurthy <me@kdheepak.com>
2020-10-31 14:59:39 -0600

    Install PyHELICS before HELICS

--- helics/build.sh
+++ helics/build.sh
@@ -16,28 +16,7 @@ echo "CONDA_PREFIX: $CONDA_PREFIX"
 echo "SRC_DIR: $SRC_DIR"
 echo "PYTHON VERSION: $PY_VER"
 
-if [ `uname` = "Darwin" ]; then
-    FLAGS="-std=c++14"
-else
-    FLAGS="-std=c++11"
-fi
-
-if [ -z "$PREFIX" ]; then
-  PREFIX="$CONDA_PREFIX"
-fi
-echo "Installing helics to ${PREFIX}"
-
-mkdir -p $SRC_DIR/helics/build && cd $SRC_DIR/helics/build
-git submodule update
-cmake $CONDA_CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=$FLAGS -DCMAKE_INSTALL_PREFIX=$PREFIX ../
-make -j $CPU_COUNT
-make install
-
-if [[ $PY3K -eq 1 || $PY3K == True || $PY3K == "True" ]]; then
-    echo "Python 3"
-else
-    echo "Python 2"
-fi
+echo "******************* Installing PyHELICS ***************"
 
 export PYTHON_BIN_PATH=${PYTHON}
 export PYTHON_LIB_PATH=${SP_DIR}
@@ -48,3 +27,21 @@ cd $SRC_DIR/pyhelics
 mkdir -p "$SP_DIR/helics/install"
 
 cp -r helics/* $SP_DIR/helics/
+
+echo "******************* Installing HELICS ***************"
+
+if [ `uname` = "Darwin" ]; then
+    FLAGS="-std=c++14"
+else
+    FLAGS="-std=c++11"
+fi
+
+if [ -z "$PREFIX" ]; then
+  PREFIX="$CONDA_PREFIX"
+fi
+
+mkdir -p $SRC_DIR/helics/build && cd $SRC_DIR/helics/build
+git submodule update
+cmake $CONDA_CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=$FLAGS -DCMAKE_INSTALL_PREFIX="$SP_DIR/helics/install" ../
+make -j $CPU_COUNT
+make install
