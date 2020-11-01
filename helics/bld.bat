e601fbd3d4c90e7601e3b301856c37474a230115
Dheepak Krishnamurthy <me@kdheepak.com>
2020-11-01 09:58:28 -0700

    Add set VS90COMNTOOLS=%VS140COMNTOOLS%

--- helics/bld.bat
+++ helics/bld.bat
@@ -13,6 +13,8 @@ if "%VisualStudioVersion%" == "" (
   set VisualStudioVersion=9
 )
 
+set VS90COMNTOOLS=%VS140COMNTOOLS%
+
 cmake -G"NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DHELICS_BUILD_TESTS=OFF -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX=%SP_DIR%\helics\install ..
 cmake --build . --config Release --target install
 
