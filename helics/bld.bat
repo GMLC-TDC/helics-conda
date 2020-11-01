fd6378e297931955579f861f96744c71a0bc2c65
Dheepak Krishnamurthy <me@kdheepak.com>
2020-11-01 08:47:21 -0700

    Remove HELICS_PLATFORM

--- helics/bld.bat
+++ helics/bld.bat
@@ -5,22 +5,10 @@ set PY_VER_NO_DOT=%PY_VER:.=%
 
 echo "Setting build options"
 
-if "%PYTHON_ARCH%" == "32" (
-    set CONDA_FORCE_32BIT=1
-    set HELICS_PLATFORM=-AWin32
-    echo "Building 32 bit"
-) ELSE (
-    set HELICS_PLATFORM=""
-    :: -Ax64
-    echo "Building 64 bit"
-)
-
-echo %HELICS_PLATFORM%
-
 mkdir "%SP_DIR%"\helics
 mkdir "%SP_DIR%"\helics\install
 
-cmake -G"Ninja" %HELICS_PLATFORM% -DCMAKE_BUILD_TYPE=Release -DHELICS_BUILD_TESTS=OFF -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX=%SP_DIR%\helics\install ..
+cmake -G"Ninja" -DCMAKE_BUILD_TYPE=Release -DHELICS_BUILD_TESTS=OFF -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX=%SP_DIR%\helics\install ..
 cmake --build . --config Release --target install
 
 if errorlevel 1 exit /b 1
